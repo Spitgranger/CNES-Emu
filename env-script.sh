d408d5ebd262d37471dc024a8fe0f5f49c735858
Richard Fan <richard.fan2000@hotmail.com>
2025-01-10 11:53:13 -0500

    feat: debugging code and script to setup environment

---
+++ env-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SESH="CNES-EMU"
+
+tmux has-session -t $SESH 2>/dev/null
+
+if [ $? != 0 ]; then
+	tmux new-session -d -s $SESH -n "editor"
+	tmux send-keys -t $SESH:editor "cd ~/Documents/Projects/gameboy" C-m
+	tmux send-keys -t $SESH:editor "nvim ." C-m
+
+	tmux new-window -t $SESH -n "cmake"
+	tmux send-keys -t $SESH:server "cd ~/Documents/Projects/gameboy" C-m
+	tmux select-window -t $SESH:editor
+fi
+tmux attach-session -t $SESH
--- src/debug.cpp
+++ src/debug.cpp
@@ -1,7 +1,27 @@
 #include "cpu.hpp"
 #include <string>
+#include <vector>
 
 std::string traceCpuState(CPU &cpu) {
+  // The format of the output string should be PC/CPU OPCODE/OPCODE IN ASS/IF
+  // INDIRECT +X OR +Y/REST OF REGISTERS/CPU PPU CLOCK CYCLES
   uint8_t opcode = cpu.readFromMemory(cpu.PC);
+  uint16_t programCounter = cpu.PC;
   CPU::instruction instruction = cpu.lookupTable[opcode];
+  std::string assemblyName = instruction.name;
+  uint8_t sizeOfInstruction = instruction.bytes;
+  std::vector<uint8_t> hexDump;
+  hexDump.push_back(opcode);
+
+  uint16_t operandAddress;
+  uint8_t operandValue;
+
+  if (instruction.mode == CPU::ADDRESSING::Immediate || instruction.mode == CPU::ADDRESSING::NoneAddressing) {
+    operandAddress = 0;
+    operandValue = 0;
+  } else {
+    operandAddress = cpu.getAbsoluteAddress(instruction.mode ,programCounter + 1);
+    operandValue = cpu.readFromMemory(operandAddress);
+  }
+  
 }
